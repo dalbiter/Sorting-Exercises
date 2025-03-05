5c87897a044e0ee9ba1045fcc8e891ea55ff0f6d
danx <rushn24@gmail.com>
2025-03-04 20:18:58 -0500

    created merge function

--- app.js
+++ app.js
@@ -21,3 +21,35 @@ function bubbleSort(arr){
     return arr; 
 }
 
+/**
+ * give two arrays of numbers returns a new array with the values from both arrays sorted
+ * 
+ * @param {Array} arr1 - an array of numbers
+ * @param {Array} arr2 - an array of numbers
+ * @returns a new array with values from both arrays sorted
+ */
+function merge(arr1, arr2){
+    const results = [];
+    let i = 0;
+    let j = 0;
+   while( i < arr1.length && j < arr2.length){
+        if(arr1[i] < arr2[j]){
+            results.push(arr1[i]);
+            i++;
+        } else {
+            results.push(arr2[j]);
+            j++;
+        }
+    }
+    while(i < arr1.length){
+        results.push(arr1[i])
+        i++
+    }
+    while(j < arr2.length){
+        results.push(arr2[j])
+        j++
+    }
+    return results;
+}
+
+
